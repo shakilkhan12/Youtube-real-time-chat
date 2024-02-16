a593687a6cdff458debea86dc9ea328d61450e2a
shakilkhan12 <shakilkhan621@gmail.com>
2024-02-16 11:04:04 +0500

    signup

--- backend/out/controller/user.controller.js
+++ backend/out/controller/user.controller.js
@@ -8,15 +8,39 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_validator_1 = require("express-validator");
+const bcrypt_1 = __importDefault(require("bcrypt"));
+const user_model_1 = __importDefault(require("../models/user.model"));
 class User {
     register(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             const errors = (0, express_validator_1.validationResult)(req);
             if (errors.isEmpty()) {
                 const { name, email, password } = req.body;
-                return res.status(200).json(req.body);
+                try {
+                    const checkUser = yield user_model_1.default.findOne({ email });
+                    if (!checkUser) {
+                        const salt = yield bcrypt_1.default.genSalt(10);
+                        const hashedPassword = yield bcrypt_1.default.hash(password, salt);
+                        const createdUser = yield user_model_1.default.create({
+                            name,
+                            email,
+                            password: hashedPassword
+                        });
+                        createdUser.password = "";
+                        return res.status(201).json({ msg: 'Your account has been created successfully', user: createdUser });
+                    }
+                    else {
+                        return res.status(400).json({ error: 'Email is already taken' });
+                    }
+                }
+                catch (error) {
+                    return res.status(500).json({ error: 'Server internal error' });
+                }
             }
             else {
                 return res.status(400).json(errors.array());
--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
+import bcrypt from "bcrypt"
+import UserModel from "../models/user.model";
 
 type BodyTypes = {
   name: string;
@@ -11,7 +13,24 @@ class User {
     const errors = validationResult(req)
     if(errors.isEmpty()) {
     const {name, email, password}: BodyTypes = req.body;
-    return res.status(200).json(req.body)
+      try {
+        const checkUser = await UserModel.findOne({email})
+        if(!checkUser) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            const createdUser = await UserModel.create({
+              name,
+              email,
+              password: hashedPassword
+            })
+            createdUser.password = ""
+            return res.status(201).json({msg: 'Your account has been created successfully', user: createdUser})
+        } else {
+          return res.status(400).json({error: 'Email is already taken'})
+        }
+      } catch (error) {
+        return res.status(500).json({error: 'Server internal error'})
+      }
     } else {
       return res.status(400).json(errors.array())
     }
